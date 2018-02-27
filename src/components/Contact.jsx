85ffef1cc2db6abefdb80a1f44cb5d3587ba08d8
titouan <titouan.creach@eegle.io>
2018-02-27 18:52:46 +0100

    add contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import { FormControl, FormHelperText } from 'material-ui/Form';
+
+const styles = theme => ({
+  root: {
+    maxWidth: '60%',
+    margin: 'auto',
+  },
+  paper: {
+    padding: 16
+  },
+});
 
 const Contact = (props) => {
+
+  const {classes} = props;
   return (
-    <div>
-      contact
+    <div className={classes.root}>
+      <Paper elevation={4} className={classes.paper}>
+        <Typography variant="headline" component="h3">
+          Get in touch
+        </Typography>
+        <FormControl fullWidth className={classes.formControl}>
+            <InputLabel htmlFor="email">email</InputLabel>
+            <Input />
+        </FormControl>
+        <FormControl fullWidth className={classes.formControl}>
+          <InputLabel htmlFor="object">object</InputLabel>
+          <Input />
+        </FormControl>
+        <FormControl fullWidth className={classes.formControl}>
+          <InputLabel htmlFor="message">Message</InputLabel>
+          <Input multiline />
+        </FormControl>
+      </Paper>
     </div>
   );
 };
 
-export default Contact;
+export default withStyles(styles)(Contact);
