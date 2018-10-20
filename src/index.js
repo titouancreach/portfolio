344d25bc4b6464a4d4730a1ca619c672c95cdc77
titouancreach <titouan.creach@gmail.com>
2018-10-20 19:15:02 +0200

    Go back to the HASH router because of gh-pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 import App from './components/App.jsx'
 import './index.css'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
